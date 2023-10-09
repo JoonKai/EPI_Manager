dea0a77a759af462029cdb73d26b9b6dd16bdc71
JoonKai <joonseok831@gmail.com>
2023-10-10 01:18:38 +0900

    1111

---
+++ EPI_Manager/Views/Layout/EPI_BottomBar.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EPI_Manager.Views.Layout
+{
+
+    public partial class EPI_BottomBar : UserControl
+    {
+        #region 디펜던시프로퍼티
+        public static readonly DependencyProperty DigitalTimeProperty = DependencyProperty.Register("DigitalTime", typeof(DateTime), typeof(EPI_BottomBar), new FrameworkPropertyMetadata(DateTime.Now));
+        public DateTime DigitalTime
+        {
+            get { return (DateTime)GetValue(DigitalTimeProperty); }
+            set { SetValue(DigitalTimeProperty, value); }
+        }
+        #endregion
+        public EPI_BottomBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
