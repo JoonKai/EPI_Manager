e23bc321382430aa4973fff3118898cae41ff9d5
JoonKai <joonseok831@gmail.com>
2023-12-05 19:49:34 +0900

    1111

--- EPI_Manager/ViewModels/PLChartViewModel.cs
+++ EPI_Manager/ViewModels/PLChartViewModel.cs
@@ -1,39 +1,11 @@
-﻿using LiveCharts.Defaults;
-using LiveCharts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EPI_Manager.ViewModels
+﻿namespace EPI_Manager.ViewModels
 {
 
     public class PLChartViewModel : ViewModelBase
     {
-
-        public ChartValues<ObservablePoint> ValuesA { get; set; }
-        public ChartValues<ObservablePoint> ValuesB { get; set; }
-        public ChartValues<ObservablePoint> ValuesC { get; set; }
-
         public PLChartViewModel()
         {
-            Initialize();
-        }
-
-        private void Initialize()
-        {
-            var r = new Random();
-            ValuesA = new ChartValues<ObservablePoint>();
-            ValuesB = new ChartValues<ObservablePoint>();
-            ValuesC = new ChartValues<ObservablePoint>();
-
-            for (var i = 0; i < 20; i++)
-            {
-                ValuesA.Add(new ObservablePoint(r.NextDouble() * 300, r.NextDouble() * 300));
-                ValuesB.Add(new ObservablePoint(r.NextDouble() * 300, r.NextDouble() * 300));
-                ValuesC.Add(new ObservablePoint(r.NextDouble() * 300, r.NextDouble() * 300));
-            }
+            
         }
     }
 }
